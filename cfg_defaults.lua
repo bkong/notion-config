3673ac7d793caafdccceeabc2214e095314c2dc6
Bailey Kong <bhkong@uci.edu>
2014-11-22 01:12:39 -0800

    Load cfg_local before everything else.

--- cfg_defaults.lua
+++ cfg_defaults.lua
@@ -2,15 +2,15 @@
 -- Notion default settings
 --
 
+if os and os.execute("test -r ~/.notion/cfg_local.lua") == 0 then
+    dopath("cfg_local") -- Anything that you don't want to share across machines
+end
+
 dopath("cfg_keyboard")
 dopath("cfg_mouse")
 dopath("cfg_menuitems")
 dopath("cfg_kludges")
 dopath("cfg_layouts")
-
-if os and os.execute("test -r ~/.notion/cfg_local.lua") == 0 then
-    dopath("cfg_local") -- Anything that you don't want to share across machines
-end
 
 dopath("mod_query")
 dopath("mod_menu")
